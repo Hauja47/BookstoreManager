93a5b179293bc37395abe479f0d57e5e7bf8cc96
hauja47 <hoangletrunghau01@gmail.com>
2022-05-17 16:51:37 +0700

    Create HibernateUtils.java

---
+++ src/main/java/com/team15/bookstoremanager/utils/HibernateUtils.java
@@ -0,0 +1,31 @@
+package com.homework.examschedulemng.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtils {
+    public static final SessionFactory sessionFactory = buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+
+            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed. " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
