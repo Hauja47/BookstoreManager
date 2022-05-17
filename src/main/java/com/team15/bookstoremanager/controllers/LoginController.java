81c663d0eec2ebec5d1957bc58243bb0d21050d0
hauja47 <hoangletrunghau01@gmail.com>
2022-05-17 16:51:15 +0700

    Remove Primary, Secondary, add Login view and controller

---
+++ src/main/java/com/team15/bookstoremanager/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package com.team15.bookstoremanager.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    void switchToSecondary(ActionEvent event) {
+
+    }
+
+}
